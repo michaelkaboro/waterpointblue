6d91f4aa2e705ed7712dc978e4b9a167b5ee9152
Michael Kaboro <michael.kaboro@gmail.com>
2019-11-06 05:49:03 +0300

    Add project files.

---
+++ WaterPointBlue.Data/SensorData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WaterPointBlue.Data
+{
+    public class SensorData
+    {
+        public Guid SiteId { get; set; }
+        public string SiteName { get; set; }
+        public string CountyName { get; set; }
+        public string DocType { get; set; }
+    }
+}
